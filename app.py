d79dc6dba7c51d7e3ea1bf04a29e8ce5bbfbd803
Jeff Thomas <jeffthomasweb@gmail.com>
2023-09-25 02:31:20 -0400

    add asyncpg support

--- app.py
+++ app.py
@@ -4,11 +4,33 @@ from starlette.routing import Route
 from starlette.templating import _TemplateResponse, Jinja2Templates
 from starlette.datastructures import FormData
 import uvicorn
+import asyncpg
 
 templates = Jinja2Templates(directory = "templates")
 
+connection_pool = None
+
+async def startdatabase():
+    global connection_pool
+    connection_pool = await asyncpg.create_pool(
+            #user and password redacted
+            user = "", 
+            password = "",
+            database = "starletteexample",
+            host = "localhost",
+            port = 5432)
+
 async def homepage(request: Request) -> _TemplateResponse:
     return templates.TemplateResponse(request, "index.html")
+
+async def database_call(request: Request) -> _TemplateResponse:
+    result = []
+
+    async with connection_pool.acquire() as connection:
+        get_query = await connection.fetch('SELECT * from dogs')
+        result.append(get_query)
+    
+    return templates.TemplateResponse(request, "example.html", {"name" : result})
 
 async def display_form(request: Request) -> _TemplateResponse:
     return templates.TemplateResponse(request, "form.html")
@@ -32,9 +54,10 @@ routes: list[Route] = [
     Route("/displayform", endpoint = display_form),
     Route("/form", endpoint = form, methods = ["POST"]),
     Route("/example", endpoint = example),
+    Route("/query", endpoint = database_call),
     ]
 
-app: Starlette = Starlette(debug = True, routes = routes)
+app: Starlette = Starlette(debug = True, routes = routes, on_startup = [startdatabase])
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "localhost", port = 8000)
